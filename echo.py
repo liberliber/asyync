6585a7d7218d253eee7d31f2a8b8db12e6579509
liberliber <53687918+liberliber@users.noreply.github.com>
2019-08-22 11:46:25 +0700

    Update echo.py

--- echo.py
+++ echo.py
@@ -7,9 +7,18 @@ cl.login(name="syncline")
 canc = ["u51e83666117c1daf018ac57e28ea53f0"]
 
 @cl.poll.hooks(type=25, filters=Filters.text)
-async def cnc_message(msg):
+async def echo_message(msg):
 
 	if text == "helo":
-		cl.talk.cancelGroupInvitation(msg.to, canc)
+		cl.talk.sendMessage(msg.to, "Hello1")
+		cl.talk.sendMessage(msg.to, "Hello2")
+		cl.talk.sendMessage(msg.to, "Hello3")
+		cl.talk.sendMessage(msg.to, "Hello4")
+		cl.talk.sendMessage(msg.to, "Hello5")
+		cl.talk.sendMessage(msg.to, "Hello6")
+		cl.talk.sendMessage(msg.to, "Hello7")
+		cl.talk.sendMessage(msg.to, "Hello8")
+		cl.talk.sendMessage(msg.to, "Hello9")
+		cl.talk.sendMessage(msg.to, "Hello10")
 
 cl.poll.streams()
